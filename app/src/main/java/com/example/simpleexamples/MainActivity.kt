d9e6bd3c40653a6a182be612dace6668bb1d4c8d
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-25 15:28:36 +0400

    feat(app): add setOnClickListener for 'button_send_name'

--- app/src/main/java/com/example/simpleexamples/MainActivity.kt
+++ app/src/main/java/com/example/simpleexamples/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.simpleexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_send_name.setOnClickListener {
+            val hello = getString(R.string.hello)
+            val name = editTextName.text.toString().trim()
+            "$hello, $name!".also { textView.text = it }
+        }
     }
 }

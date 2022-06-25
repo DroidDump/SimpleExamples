0c9b59f85d5018dce49d556584c3f466401d1d86
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-25 18:22:57 +0400

    feat(app): add attr: 'android:onClick' and 'fun sendAge()' for 'button_send_age'

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Simple Examples
 
 
-1. A simple example of assigning an OnClickListener to a button  
-   (button="@+id/button_send_name")
+A simple example of assigning an OnClickListener to a button
+
+(button="@+id/button_send_name")  
+using kotlinx.synthetic and View.setOnClickListener()
+
+(button="@+id/button_send_age")  
+@deprecated  
+using attr: android:onClick="funcName"
--- app/src/main/java/com/example/simpleexamples/MainActivity.kt
+++ app/src/main/java/com/example/simpleexamples/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simpleexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -15,5 +16,11 @@ class MainActivity : AppCompatActivity() {
             val name = editTextName.text.toString().trim()
             "$hello, $name!".also { textView.text = it }
         }
+    }
+
+    fun sendAge(@Suppress("UNUSED_PARAMETER") view: View) {
+        val age = editTextAge.text.toString().trim()
+        val yourAge = getString(R.string.your_age)
+        "$yourAge: $age".also { textView.text = it }
     }
 }
